343a2c97a186353f0fcc2fb1144bbfed9192a7c2
unknown <marieaureliza@outlook.fr>
2023-04-17 15:59:05 +0200

    [T002] suppr corssorigin annotation

--- elba/src/main/java/fr/liza/elba/web/StarterController.java
+++ elba/src/main/java/fr/liza/elba/web/StarterController.java
@@ -2,12 +2,10 @@ package fr.liza.elba.web;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/starter")
 public class StarterController {
