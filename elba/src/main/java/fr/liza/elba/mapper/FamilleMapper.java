cd95c6407c016eab3c73ac41f31a7c0421c3dcd3
unknown <marieaureliza@outlook.fr>
2023-05-13 14:23:52 +0200

    [T010] ajout WS et familleDto

---
+++ elba/src/main/java/fr/liza/elba/mapper/FamilleMapper.java
@@ -0,0 +1,17 @@
+package fr.liza.elba.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import fr.liza.elba.model.dto.FamilleDto;
+import fr.liza.elba.model.jpa.Famille;
+
+@Mapper
+public interface FamilleMapper {
+
+	FamilleDto toDto(Famille famille);
+
+	List<FamilleDto> toDtoList(List<Famille> familles);
+
+}
--- elba/src/main/java/fr/liza/elba/web/StarterController.java
+++ elba/src/main/java/fr/liza/elba/web/StarterController.java
@@ -3,7 +3,6 @@ package fr.liza.elba.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import fr.liza.elba.model.dto.SimDto;
+import fr.liza.elba.service.CreerFamilleService;
 import fr.liza.elba.service.StarterService;
-import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping("/api/starter")
@@ -24,6 +22,9 @@ public class StarterController {
 
 	@Autowired
 	private StarterService starterService;
+
+	@Autowired
+	private CreerFamilleService creerFamilleService;
 
 	@GetMapping("/generer/{nombre}")
 	public void genererStarters(@PathVariable("nombre") Integer nombre) {
@@ -35,7 +36,6 @@ public class StarterController {
 	public SimDto completerStarter(@RequestBody SimDto simDto) {
 		
 		return starterService.completerStarter(simDto);
-		
 	}
 	
 	@GetMapping("/liste-groupes")
@@ -48,5 +48,12 @@ public class StarterController {
 	public List<SimDto> chargerGroupe(@PathVariable("numero") Integer numero) {
 
 		return starterService.chargerGroupe(numero);
+	}
+
+	@PostMapping("/transformer-groupe/{numero}")
+	public void transformerGroupe(@PathVariable("numero") Integer numero) {
+
+		creerFamilleService.transformerGroupe(numero);
+		System.out.println("jkljkljkl");
 	}
 }
